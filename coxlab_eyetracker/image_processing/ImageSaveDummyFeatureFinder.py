c175634d7f18bc4b152807b3ccf913047680f4db
David Cox <david.daniel.cox@gmail.com>
2012-11-15 20:35:54 -0500

    Forgot to add

---
+++ coxlab_eyetracker/image_processing/ImageSaveDummyFeatureFinder.py
@@ -0,0 +1,54 @@
+from EyeFeatureFinder import EyeFeatureFinder
+import cPickle as pkl
+import os.path
+import os
+import uuid
+import time
+
+
+class ImageSaveDummyFeatureFinder(EyeFeatureFinder):
+
+    def __init__(self, real_ff, path):
+        self.real_ff = real_ff
+        self.session_key = time.strftime('%Y-%m-%d-%H%M-%S',time.localtime(time.time())) + '_' + str(uuid.uuid1())
+        self.n_frames = 0
+        self.frames_per_dir = 1000
+
+        self.base_path = os.path.expanduser(path)
+        self.base_path += '/' + self.session_key
+
+        os.makedirs(self.base_path)
+
+        self.current_path = None
+
+    def analyze_image(self, image, guess=None, **kwargs):
+        if guess is not None and 'timestamp' in guess:
+            self.save_image(image, guess['timestamp'])
+        else:
+            print('Cannot save to disk without timestamp set')
+
+        return self.real_ff.analyze_image(image, guess, **kwargs)
+
+    def get_result(self):
+        return self.real_ff.get_result()
+
+    def save_image(self, image, timestamp):
+
+        if (self.n_frames % self.frames_per_dir == 0 or
+            self.current_path is None):
+
+            # make a new directory
+            self.current_path = (self.base_path + '/' +
+                                '%.10d' % (self.n_frames /
+                                           self.frames_per_dir))
+            print(self.current_path)
+            os.mkdir(self.current_path)
+
+        fname = '%s/%d.pkl' % (self.current_path,
+                               timestamp)
+
+        with open(fname, 'w') as f:
+            self.n_frames += 1
+            pkl.dump(image, f)
+
+
